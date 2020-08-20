7fb5b58c4e4065dfb5ef4531905ca668c7691d72
danny galimi <dannygalimi13@aim.com>
2020-08-20 01:43:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def line(people)
+  if people.size == 0
+  puts "The line is currently empty."
+  elsif people.size >= 1
+  status = "The line is currently:"
+  
+  people.each_with_index do |person,index| 
+    status << " #{index+1}. #{person}"
+  end
+puts status
+end
+end
