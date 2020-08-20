2901caeab56dbaa0d08adeb93494eacd2ecdb633
danny galimi <dannygalimi13@aim.com>
2020-08-20 01:59:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,6 +20,6 @@ def now_serving(line)
   if line.size == 0
   puts "There is nobody waiting to be served!"
   elsif line.size >= 1
-puts "Currently serving #{line[0]}"
+puts "Currently serving #{line[0]}."
 end
 end
